e2c98be86c37f45c24edb92f1fc56003444d5dd5
NastyaPryadieva <nastya.pryadieva@yandex.ru>
2022-05-19 18:13:14 +0300

    новый файл

---
+++ что то.py
@@ -0,0 +1,15 @@
+impont ramdom
+
+
+
+
+#print("Как тебя зовут?")
+#myName = input()
+#print('Привет, '+myName)
+#print('Чем ты занимаешься' +myName+ 'Я занимаюсь програмированием!')
+#hobby = input()
+#print(hobby+ 'Ухты,интересное занятие!')
+#print(zanyatie+'А какое твоё хобби?')
+#zanyatie = input()
+x = ramdom.ramdint(1,20)
+print('Похоже,сгенерировано значение!'+ str(x))
